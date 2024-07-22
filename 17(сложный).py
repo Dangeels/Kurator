d7760dc818766042e511810057061b8f2eaa97c0
Dangeels <114171101+Dangeels@users.noreply.github.com>
2024-07-22 22:44:55 +0300

    1

--- 17(сложный).py
+++ 17(сложный).py
@@ -1,3 +1,4 @@
+from math import ceil
 f = open('17(сложный)')
 
 frame_time = int(f.readline())
@@ -15,10 +16,12 @@ for i in range(len(arr)-3):
     effect = abs_dct[min(v)] if min(v)!=0 else abs_dct[v[1]]
     dodge = abs_dct[v[2]]
     attack_time = abs_dct[v[1]] if min(v)!=0 else abs_dct[v[0]]
-    if attack_time!=0 and frame_time-(attack_time+dodge)>=0:
-        hp-=dmg
+    if attack_time!=0 and frame_time-(attack_time+dodge)>=0 and not defeated:
         if dmg>0:
             count_attacks+=1
+            hp-=ceil(dmg*(effect/13))
+        else:
+            hp-=ceil(dmg/(effect/13))
     if hp<=0:
         defeated=True
 print(count_attacks, hp, defeated)
