16f0f8f3c15d5b37f5e63d1ced5a986215427fab
Dangeels <114171101+Dangeels@users.noreply.github.com>
2024-07-23 05:03:18 +0300

    1

--- 17(средний).py
+++ 17(средний).py
@@ -7,6 +7,9 @@ deaths = 0
 count_days = 0
 soldiers = 0
 
+def dividers(n):
+    return [i for i in range(2,n//2+1) if n%i==0] if n>2 else 0
+
 for i in range(len(arr)-2):
     v = sorted(arr[i:i+3])
     arrows+=max(v)
@@ -16,7 +19,7 @@ average = deaths/arrows
 
 for i in range(len(arr)-2):
     v = sorted(arr[i:i+3])
-    if v[1]/v[2] > average:
+    if v[1]/v[2] > average and any(i in dividers(v[1]) for i in dividers(v[0])):
         count_days+=1
         soldiers+=v[1]
 print(count_days, soldiers)
